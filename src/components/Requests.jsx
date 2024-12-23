034ea6ee13376d1609f03cd8e1e76c709c84277c
Ronik <ronik.kanalouria@gmail.com>
2024-12-23 17:43:28 +0530

    feature : connection page

---
+++ src/components/Requests.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Requests = () => {
+  return (
+    <>
+      Requests
+    </>
+  )
+}
+
+export default Requests
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "./UserSclice";
 import feedReducer from "./feedSlice";
+import connectionReducer from "./connectionSlice";
+import requestReducer from "./requestSlice";
 
 const appStore = configureStore({
   reducer: {
     user: useReducer,
     feed: feedReducer,
+    connections: connectionReducer,
+    requests: requestReducer,
   },
 });
 
