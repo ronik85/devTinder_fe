99e4a530ba82299a6433c5a72ce114aaf72e5a6f
Ronik <ronik.kalanouria@gmail.com>
2024-12-23 19:50:53 +0530

    Fixes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("Elon@gmail.com");
-  const [password, setPassword] = useState("Elon@0085");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [isLoginForm, setIsLoginForm] = useState(true);
