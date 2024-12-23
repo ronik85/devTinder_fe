11232cd1da056e53fece2b9caf10cc6fae27c9ee
Ronik <ronik.kanalouria@gmail.com>
2024-12-23 16:21:53 +0530

    feed added

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "./UserSclice";
+import feedReducer from "./feedSlice";
 
 const appStore = configureStore({
   reducer: {
     user: useReducer,
+    feed: feedReducer,
   },
 });
 
